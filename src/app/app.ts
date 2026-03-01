5aed6a189131dc0d8fd4d2a51e2222964ab916d4
matheeshapr <matheeshap62@gmail.com>
2026-03-01 09:25:06 +0530

    refactor: update app configuration and remove placeholder content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// src/app/app.ts
+import { Component } from '@angular/core';
+import { Navbar } from './components/navbar/navbar'; 
+import { SlotGrid } from './components/slot-grid/slot-grid'; // file name එක චෙක් කරන්න
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Navbar, SlotGrid], // මෙතන තියෙන නම් දෙක පහළ පන්ති වල නම් වලට සමාන විය යුතුයි
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('ibs-frontend');
-}
+export class App { }
