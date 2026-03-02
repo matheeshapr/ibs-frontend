d7bb122175bb7f15b558fedc226239ab35f8bb2d
matheeshapr <matheeshap62@gmail.com>
2026-03-02 19:11:43 +0530

    refactor: remove slot-grid component implementation and simplify to placeholder

--- src/app/components/slot-grid/slot-grid.ts
+++ src/app/components/slot-grid/slot-grid.ts
@@ -1,33 +1,11 @@
-// src/app/components/slot-grid/slot-grid.component.ts
+import { Component } from '@angular/core';
 
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-// අන්තිමට තිබ්බ .service කෑල්ල අයින් කරන්න
-import { BookingService } from '../../services/booking';
 @Component({
   selector: 'app-slot-grid',
-  standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './slot-grid.html',
-  styleUrl: './slot-grid.css'
+  styleUrl: './slot-grid.css',
 })
-export class SlotGrid implements OnInit {
-  slots: any[] = [];
+export class SlotGrid {
 
-  // මෙතන 'BookingService' කියන නම දැන් නිවැරදිව පෙනේවි
-  constructor(private bookingService: BookingService) {} 
-
-  ngOnInit() {
-    this.loadSlots();
-  }
-
-  loadSlots() {
-    this.bookingService.getAllSlots().subscribe(res => {
-      this.slots = res;
-    });
-  }
-
-  onBook(slotId: number) {
-    console.log("Selected Slot ID:", slotId);
-  }
 }
