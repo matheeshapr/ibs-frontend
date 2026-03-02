6b905dc395f9d3a404ca0b888f14fda72adf169b
matheeshapr <matheeshap62@gmail.com>
2026-03-02 19:08:49 +0530

    fix: correct component imports and remove unused SlotGrid from app template

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
-// src/app/app.ts
 import { Component } from '@angular/core';
-import { Navbar } from './components/navbar/navbar'; 
-import { SlotGrid } from './components/slot-grid/slot-grid'; // file name එක චෙක් කරන්න
+import { NavbarComponent } from './components/navbar/navbar';
+import { SlotGrid } from  './components/slot-grid/slot-grid';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Navbar, SlotGrid], // මෙතන තියෙන නම් දෙක පහළ පන්ති වල නම් වලට සමාන විය යුතුයි
+  imports: [ NavbarComponent, SlotGrid],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App { }
+export class AppComponent {}
