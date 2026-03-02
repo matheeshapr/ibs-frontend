852f1c1057ece509250c02fd7160fe7d0e530ee8
matheeshapr <matheeshap62@gmail.com>
2026-03-02 19:08:15 +0530

    refactor: clean up app configuration and update main application bootstrap

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), // මේක අනිවාර්යයි
     provideRouter(routes),
     provideHttpClient()
   ]
