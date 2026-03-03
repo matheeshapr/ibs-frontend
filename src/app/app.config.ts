d96bcf2c9df31ba809798e56c8ded556ef9c7049
matheeshapr <matheeshap62@gmail.com>
2026-03-03 09:27:00 +0530

    feat: enhance app configuration and add Material theming support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),       // ✅ Backend connect වෙන්න
+    provideAnimations(),       // ✅ Material animations
   ]
 };
