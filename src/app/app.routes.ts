c40ea2bf2c417d89b5cb3ca583ac860fef263e9f
matheeshapr <matheeshap62@gmail.com>
2026-03-03 09:27:11 +0530

    feat: implement booking routes and update app structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CalendarViewComponent } from './components/calendar-view/calendar-view';
+import { BookingList } from './components/booking-list/booking-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'slots', pathMatch: 'full' },
+  { path: 'slots', component: CalendarViewComponent },
+  { path: 'bookings', component: BookingList },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './components/navbar/navbar';
-import { SlotGrid } from  './components/slot-grid/slot-grid';
-
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ NavbarComponent, SlotGrid],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule],
+  template: `
+    <mat-toolbar color="primary">
+      <span>🎯 Interview Booking System</span>
+      <span style="flex:1"></span>
+      <button mat-button routerLink="/slots">Slots</button>
+      <button mat-button routerLink="/bookings">My Bookings</button>
+    </mat-toolbar>
+
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {}
