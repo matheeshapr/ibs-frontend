34e27be8bb3f791ee477773ff4940abba31e9615
matheeshapr <matheeshap62@gmail.com>
2026-03-04 22:38:38 +0530

    refactor: remove routing and main app component code

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CalendarViewComponent } from './components/calendar-view/calendar-view';
-import { BookingList } from './components/booking-list/booking-list';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'slots', pathMatch: 'full' },
-  { path: 'slots', component: CalendarViewComponent },
-  { path: 'bookings', component: BookingList },
-];
