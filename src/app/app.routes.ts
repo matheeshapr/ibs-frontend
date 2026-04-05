aa533e06b9ac0f52fdff5b1af1eefa06c36b9293
matheeshapr <matheeshap62@gmail.com>
2026-04-05 09:19:01 +0530

    feat: update routing configuration to redirect to login and enhance initial navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login';
 import { InterviewerListComponent } from './components/interviewer-list/interviewer-list';
 import { BookingPageComponent } from './components/booking-page/booking-page';
 import { MyBookingsComponent } from './components/my-bookings/my-bookings';
 
 export const routes: Routes = [
-  { path: '', component: InterviewerListComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+  { path: 'login', component: LoginComponent },
+  { path: 'interviewers', component: InterviewerListComponent },
   { path: 'booking/:id', component: BookingPageComponent },
   { path: 'my-bookings', component: MyBookingsComponent }, 
-  { path: '**', redirectTo: '' } 
+  { path: '**', redirectTo: 'login' } 
 ];
