887611a2bd7ad0b8e8179bdb2cb13ccbe674bb17
matheeshapr <matheeshap62@gmail.com>
2026-04-05 09:19:09 +0530

    feat: implement dynamic navbar visibility based on routing events

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar';
 import { MyBookingsComponent } from "./components/my-bookings/my-bookings";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MyBookingsComponent], 
+  imports: [RouterOutlet, NavbarComponent, MyBookingsComponent, CommonModule ], 
   templateUrl: './app.html',
   styleUrls: ['./app.css'] 
 })
 export class AppComponent {
-  title = 'ibs-frontend';
+  showNavbar: boolean = true;
+
+constructor(private router: Router) {
+  this.router.events.subscribe((event) => {
+    if (event instanceof NavigationEnd) {
+      const currentUrl = event.urlAfterRedirects;
+      
+      if (currentUrl === '/' || currentUrl.includes('/login') || currentUrl.includes('/register')) {
+        this.showNavbar = false;
+      } else {
+        this.showNavbar = true;
+      }
+      
+      console.log('Current URL:', currentUrl, 'Show Navbar:', this.showNavbar);
+    }
+  });
+}
 }
